32f4d2e3bb0e52fe51a79c770e612a87ead28fa3
Oleksandr Minenko <OMinenko@luxoft.com>
2018-11-08 17:15:40 +0200

    fix gtest

---
+++ test/CTransformLine_test.cpp
@@ -0,0 +1,17 @@
+/*
+ * CTransformLine_test.cpp
+ *
+ *  Created on: Nov 8, 2018
+ *      Author: ominenko
+ */
+#ifdef TEST
+#include "../CTransformLine.h"
+#include "gtest/gtest.h"
+
+class CTransformLineTest: public testing::Test {
+
+};
+
+TEST_F (CTransformLineTest, deserialize) {
+}
+#endif
